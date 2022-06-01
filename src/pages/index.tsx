7042b83ac4c7261731ef14dec380813edbf27d5c
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-06-01 17:53:44 -0400

    :construction: building: home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,23 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>Hello World!</h1>
+
+      <main>
+        <section>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
     </>
   )
 }
