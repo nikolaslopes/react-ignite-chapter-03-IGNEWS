44696fa10195f8ebfb2fc88ec5c6412f11c907cb
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-06-01 16:50:38 -0400

    :sparkles: feat: header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import styles from './styles.module.scss'
 
 export function Header() {
@@ -8,7 +7,9 @@ export function Header() {
         <img src="/images/logo.svg" alt="ig.news" />
 
         <nav>
-          <a href="">Home</a>
+          <a href="" className={styles.active}>
+            Home
+          </a>
           <a href="">Posts</a>
         </nav>
       </div>
