c24199ba287b48b8728ba7b8bb4cf25828feaa70
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-06-01 20:42:45 -0400

    :sparkles: feat: stripe integration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
 import styles from './home.module.scss'
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
+import { GetServerSideProps } from 'next'
+import { stripe } from '../services/stripe'
 
-export default function Home() {
+interface IHome {
+  product: {
+    priceId: string
+    amount: number
+  }
+}
+
+export default function Home({ product }: IHome) {
   return (
     <>
       <Head>
@@ -17,7 +26,7 @@ export default function Home() {
           </h1>
           <p>
             Get access to all publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
           <SubscribeButton />
         </section>
@@ -26,4 +35,24 @@ export default function Home() {
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1L61KNLHR9hoaA94B98NtW9P', {
+    expand: ['product'],
+  })
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      product,
+    },
+  }
 }
