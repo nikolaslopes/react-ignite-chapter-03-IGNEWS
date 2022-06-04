20986b94df7fb09a1114d1fd4145c8f4c4edfbc0
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-06-03 20:37:48 -0400

    :fire: created next auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -4,7 +4,7 @@ import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 
 export function SignInButton() {
-  const isUserLoggedIn = true
+  const isUserLoggedIn = false
 
   return isUserLoggedIn ? (
     <button className={styles.signInButton} type="button">
--- src/pages/api/users/index.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-
-export default (request: NextApiRequest, response: NextApiResponse) => {
-  console.log(request.query)
-
-  const users = [{ id: 1, name: 'Nikolas' }]
-
-  return response.json(users)
-}
