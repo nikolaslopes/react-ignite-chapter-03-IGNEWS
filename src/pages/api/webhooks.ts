0ee146970b796541dc1d7a2fbd28458580b5dc99
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-06-08 23:10:33 -0400

    :fire: created update subscriptions

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -21,7 +21,12 @@ export const config = {
   },
 }
 
-const relevantEvents = new Set(['checkout.session.completed'])
+const relevantEvents = new Set([
+  'checkout.session.completed',
+  'customer.subscriptions.created',
+  'customer.subscriptions.updated',
+  'customer.subscriptions.deleted',
+])
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
@@ -45,12 +50,25 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (relevantEvents.has(type)) {
       try {
         switch (type) {
+          case 'customer.subscriptions.created':
+          case 'customer.subscriptions.updated':
+          case 'customer.subscriptions.deleted':
+            const subscription = event.data.object as Stripe.Subscription
+
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString(),
+              type === 'customer.subscriptions.created'
+            )
+
+            break
           case 'checkout.session.completed':
             const checkoutSession = event.data.object as Stripe.Checkout.Session
 
             await saveSubscription(
               checkoutSession.subscription.toString(),
-              checkoutSession.customer.toString()
+              checkoutSession.customer.toString(),
+              true
             )
             break
           default:
