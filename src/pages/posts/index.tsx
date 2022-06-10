c382a1ede0af8ec34ac71f32591abff1133e5718
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-06-09 23:35:58 -0400

    :fire: created posts content

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,9 +1,49 @@
+import Head from 'next/head'
 import styles from './styles.module.scss'
 
 export default function Posts() {
   return (
     <>
-      <h1 className={styles.header}>pinto</h1>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a href="">
+            <time>12 de março de 2021</time>
+            <strong>Lorem ipsum dolor sit ame.</strong>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+              accusantium sint reiciendis amet fugit! Quod, mollitia fugiat
+              earum distinctio error qui, quas vel nisi et maiores obcaecati hic
+              illo nulla.
+            </p>
+          </a>
+
+          <a href="">
+            <time>12 de março de 2021</time>
+            <strong>Lorem ipsum dolor sit ame.</strong>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+              accusantium sint reiciendis amet fugit! Quod, mollitia fugiat
+              earum distinctio error qui, quas vel nisi et maiores obcaecati hic
+              illo nulla.
+            </p>
+          </a>
+
+          <a href="">
+            <time>12 de março de 2021</time>
+            <strong>Lorem ipsum dolor sit ame.</strong>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+              accusantium sint reiciendis amet fugit! Quod, mollitia fugiat
+              earum distinctio error qui, quas vel nisi et maiores obcaecati hic
+              illo nulla.
+            </p>
+          </a>
+        </div>
+      </main>
     </>
   )
 }
