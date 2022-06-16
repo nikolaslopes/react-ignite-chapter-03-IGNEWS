a8656e755d6e3f360710ba78ac4ae77156d3f668
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-06-16 00:37:52 -0400

    :fire: created server props in posts

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -30,8 +30,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         email: session.user.email,
       })
 
-      console.log('stripecus', stripeCustomer)
-
       await fauna.query(
         q.Update(q.Ref(q.Collection('users'), user.ref.id), {
           data: {
