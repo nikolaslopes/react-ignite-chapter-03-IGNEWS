92027f1cb9888a1ce2e824da1ea849fde5f6c612
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-06-18 17:02:43 -0400

    :fire: difines fallback and paths

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from 'next'
+import { GetStaticPaths, GetStaticProps } from 'next'
 import { useSession } from 'next-auth/react'
 import Head from 'next/head'
 import Link from 'next/link'
@@ -57,7 +57,7 @@ export default function PostPreview({ post }: IPostPreview) {
   )
 }
 
-export const getStaticPaths = () => {
+export const getStaticPaths: GetStaticPaths = async () => {
   return {
     paths: [],
     fallback: 'blocking',
@@ -89,5 +89,6 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     props: {
       post,
     },
+    redirect: 60 * 30, // 30 minutes
   }
 }
