bdf55e67d86f810df13eefe23303e4c6eab3cd34
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-06-18 16:25:19 -0400

    :fire: created session redirect

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,8 +1,10 @@
 import { GetStaticProps } from 'next'
-import { getSession } from 'next-auth/react'
+import { useSession } from 'next-auth/react'
 import Head from 'next/head'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { RichText } from 'prismic-dom'
+import { useEffect } from 'react'
 import { getPrismicClient } from '../../../services/prismic'
 
 import styles from '../post.module.scss'
@@ -17,6 +19,15 @@ interface IPostPreview {
 }
 
 export default function PostPreview({ post }: IPostPreview) {
+  const { data: session } = useSession()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (session?.activeSubscription) {
+      router.push(`/posts/${post.slug}`)
+    }
+  }, [session])
+
   return (
     <>
       <Head>
